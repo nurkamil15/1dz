eaf0b6659992e1374c6f68bf0a43749944512685
nurkamil15 <nurkamiljumamudunov@gmail.com>
2024-03-23 15:39:51 +0600

    dz1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
+function Header () {
+  return(
+      <>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <Title/>
+      </>
+  )
+}
+
+
+function Content() {
+  return (
+      <>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, dicta.</h1>
+        <Title/>
+      </>
+  )
+}
+
+function Footer() {
+  return (
+      <>
+        <h1>bye</h1>
+        <Title/>
+      </>
+  )
+}
+
+function Title() {
+  return (
+      <div>
+        <h1>hello</h1>
+      </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Header/>
+        <Content/>
+        <Footer/>
+      </div>
   );
 }
 
